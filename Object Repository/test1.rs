09601342fce0ed25fd3095b5dfd2f34dd67ea816
BBmgpark <110453550+BBmgpark@users.noreply.github.com>
2022-11-07 16:20:55 +0900

    testmg

---
+++ Object Repository/test1.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test1</name>
+   <tag></tag>
+   <elementGuidId>87a9ff87-8f2c-4fe5-8011-5aac9cfcc887</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;repo-content-pjax-container&quot;]/div/div/div[3]/div[1]/aside/div/div[2]/div[2]/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;repo-content-pjax-container&quot;]/div/div/div[3]/div[1]/aside/div/div[2]/div[2]/a</value>
+      <webElementGuid>6ad08dc8-d094-4b10-b330-69b6c913bb3f</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/test3.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test3</name>
+   <tag></tag>
+   <elementGuidId>e5f9aaee-d9f5-44e3-84fb-7d0479d03c9a</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>

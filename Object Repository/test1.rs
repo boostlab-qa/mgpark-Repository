64f960aaf878b5aab6ec6cd2218278ea35217dfa
BBjwlee <llljjj123@bbros.kr>
2022-11-07 15:52:03 +0900

    testjw

---
+++ Object Repository/test1.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test1</name>
+   <tag></tag>
+   <elementGuidId>1f321264-c5f7-43d3-b7ec-bfb740510d11</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/test2.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test2</name>
+   <tag></tag>
+   <elementGuidId>efd7aa33-3749-4874-b383-802c068087e6</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
